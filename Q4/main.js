9b4673f5a18e224cee47f9df51c81478bf81f3f9
Puneeth Jain S <puneethjains2005@gmail.com>
2026-03-05 20:53:50 +0530

    solved q4

---
+++ Q4/main.js
@@ -0,0 +1,14 @@
+console.log("1 to 20");
+for (var i = 1; i <= 20; i++) {
+    console.log(i);
+}
+console.log("even numbers between 1 to 20");
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+console.log("Multiplication table of 5");
+for (var i = 1; i <= 10; i++) {
+    console.log(5 * i);
+}
---
+++ Q4/main.ts
@@ -0,0 +1,16 @@
+console.log("1 to 20")
+for(let i = 1;i<=20;i++){
+    console.log(i);
+}
+
+console.log("even numbers between 1 to 20")
+for(let i = 1;i<=20;i++){
+    if(i%2==0) {
+        console.log(i);
+    }
+}
+
+console.log("Multiplication table of 5");
+for(let i = 1;i<=10;i++){
+    console.log(5*i);
+}
