18de0a0154487f9805af434308840d4f355c8288
Puneeth Jain S <puneethjains2005@gmail.com>
2026-03-05 20:48:09 +0530

    solved q3

---
+++ Q3/main.js
@@ -0,0 +1,6 @@
+function checkOddOrEven(a) {
+    return a % 2 == 0 ? true : false;
+}
+console.log(checkOddOrEven(2));
+console.log(checkOddOrEven(3));
+console.log(checkOddOrEven(4));
---
+++ Q3/main.ts
@@ -0,0 +1,7 @@
+function checkOddOrEven(a:number) :boolean{
+    return a%2 == 0 ? true : false;
+}
+
+console.log(checkOddOrEven(2))
+console.log(checkOddOrEven(3))
+console.log(checkOddOrEven(4))
