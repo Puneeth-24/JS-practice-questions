122370fa29156aa30a7790b5b03719b22e84dc7d
Puneeth Jain S <puneethjains2005@gmail.com>
2026-03-05 20:29:52 +0530

    solved q1

---
+++ Q1/main.js
@@ -0,0 +1,3 @@
+var student_name = "Puneeth Jain S";
+var student_age = 20;
+console.log("Hello ".concat(student_name, ", you are ").concat(student_age, " years old."));
---
+++ Q1/main.ts
@@ -0,0 +1,3 @@
+let student_name : string = "Puneeth Jain S";
+let student_age : number = 20;
+console.log(`Hello ${student_name}, you are ${student_age} years old.`)
